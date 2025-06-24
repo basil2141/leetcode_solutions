ea0eb617ef723791916fe2fbfed0a37d8e4f15b4
basil2141 <basilmansuri4@gmail.com>
2025-06-24 18:28:39 +0530

    Time: 1 ms (93.32%), Space: 44.4 MB (91.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,26 @@
-import java.util.List;
-import java.util.ArrayList;
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, new ArrayList<Integer>(), result, new boolean[nums.length]);
-        
-        return result;
-    }
-    public void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result, boolean[] used){
-        if(nums.length==current.size()){
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        for(int i=0; i<nums.length; i++){
-            if(!used[i]){
-                used[i] = true;
-                current.add(nums[i]);
-                backtrack(nums, current, result, used);
-                used[i] = false;
-                current.remove(current.size()-1);
+    List< List<Integer>> result = new ArrayList<>();
+    Queue<List<Integer>> permutations = new LinkedList<>();
+
+    permutations.add(new ArrayList<>());
+
+    for(int num : nums){
+        int n = permutations.size();
+
+        for(int i=0; i<n; i++){
+            List<Integer> oldPer = permutations.poll();
+
+            for(int j=0; j<= oldPer.size(); j++){
+                List<Integer> newPer = new ArrayList<>(oldPer);
+
+                newPer.add(j, num);
+
+                if(newPer.size() == nums.length) result.add(newPer);
+                else permutations.add(newPer);
             }
         }
+    }
+    return result;
     }
 }
